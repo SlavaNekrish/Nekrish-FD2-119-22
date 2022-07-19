ef4e4b63f4f159bc7bebe1bdbab7fe2d9d711e8a
SlavaNekrish <vnekrish@mail.ru>
2022-07-20 00:57:14 +0300

    HW3 N.07 Vowels

--- lessons/script.js
+++ lessons/script.js
@@ -1,3 +1,11 @@
-let a = 1;
-let b = 2;
-console.log(a);
+function func(arr) {
+	for (let elem in arr) {
+		if (typeof elem == 'object') {
+			func(elem);
+		} else {
+			console.log(elem);
+		}
+	}
+}
+
+func({a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, m: {n: 8, o: 9}}}});
